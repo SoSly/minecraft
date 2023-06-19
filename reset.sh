34f51d3aaf4e91d2e3eead1b15d870222ba18813
Kevin Kragenbrink <kevin@writh.net>
2023-06-19 09:53:45 -0700

    scripts: add reset.sh

---
+++ reset.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+# Forge requires a configured set of both JVM and program arguments.
+# Add custom JVM arguments to the user_jvm_args.txt
+# Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
+#  pass them to this script directly
+
+rm -r bluemap crash-reports journeymap logs patchouli_books resourcepacks schematics screenshots simplebackups world
