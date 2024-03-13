28e36dc23130ed4762a834cf7e2c5279dfc29069
Kevin Kragenbrink <kevin@writh.net>
2024-03-13 04:46:29 +0000

    update reset script

--- reset.sh
+++ reset.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+rm -r bluemap
 rm -r world
 mkdir -p world/datapacks
 cp ~/SoSly.zip world/datapacks/
+cp ~/resourcepacks/idasbop* world/datapacks
+cp ~/resourcepacks/instrong_endrem* world/datapacks
