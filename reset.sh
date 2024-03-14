97c29b96aafb4325712f3c65daeaa8a9783206e9
Kevin Kragenbrink <kevin@writh.net>
2024-03-14 03:05:31 +0000

    update reset

--- reset.sh
+++ reset.sh
@@ -5,6 +5,6 @@ rm -r crash-reports
 rm -r logs
 mkdir -p world/datapacks
 #cp ~/SoSly.zip world/datapacks/
-cp ~/resourcepacks/idasbop* world/datapacks
-cp ~/resourcepacks/instrong_endrem* world/datapacks
-cp ~/resourcepacks/navigable-rivers* world/datapacks
+cp resourcepacks/idasbop* world/datapacks
+cp resourcepacks/instrong_endrem* world/datapacks
+cp resourcepacks/navigable-rivers* world/datapacks
