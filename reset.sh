a2be5c7200046838b54270c505bed1367ddba622
Kevin Kragenbrink <kevin@writh.net>
2024-03-13 20:04:29 -0700

    add wwoo and update other mods

--- reset.sh
+++ reset.sh
@@ -4,6 +4,7 @@ rm -r world
 rm -r crash-reports
 rm -r logs
 mkdir -p world/datapacks
-cp ~/SoSly.zip world/datapacks/
+#cp ~/SoSly.zip world/datapacks/
 cp ~/resourcepacks/idasbop* world/datapacks
 cp ~/resourcepacks/instrong_endrem* world/datapacks
+cp ~/resourcepacks/navigable-rivers* world/datapacks
